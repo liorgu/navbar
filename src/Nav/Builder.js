7aa0a599e3b3aa342609218ffcdf0593ff8deb69
liorgu <lior.gutweter@gmail.com>
2021-05-17 10:24:14 +0300

    Clean

--- src/Nav/Builder.js
+++ src/Nav/Builder.js
@@ -1,18 +1,20 @@
 import Nav from "./Nav";
 import { Divider } from "semantic-ui-react";
-import MenuGroupItem from "./MenuGroupItem";
-import MenuItem from "./MenuItem";
 
 const Builder = ({ items }) => (
   <Nav>
     <Nav.Menu>
       {items.map((item) => {
         const { subItems, ...itemProps } = item;
-        const MenuItemComponent = subItems ? MenuGroupItem : MenuItem;
+        const MenuItemComponent = subItems ? Nav.MenuGroupItem : Nav.MenuItem;
         return (
           <MenuItemComponent key={itemProps.text} {...itemProps}>
             {(subItems || []).map((subItem) => (
-              <MenuItem key={subItem.text} {...subItem} isSubItem></MenuItem>
+              <Nav.MenuItem
+                key={subItem.text}
+                {...subItem}
+                isSubItem
+              ></Nav.MenuItem>
             ))}
           </MenuItemComponent>
         );
